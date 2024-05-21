fb63ccca808db2e928b22c909666cf2f59510385
cnitin041 <70575293+cnitin041@users.noreply.github.com>
2024-05-22 00:11:51 +0530

    bitwise operators

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    cin>>a>>b;
-    for(int i=0;i<a+1;i++){
-        for(int j=1;j<=i;j++){
-            cout<<" "<<j;
-        }
-        cout<<endl;
-    }
+    int a=4;
+    int b=6;
+    cout<<"&"<< a&b <<endl;
+
 }
