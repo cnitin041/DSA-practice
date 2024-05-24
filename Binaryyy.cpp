8b8f3a333aec86a1525b18b568e399155142e953
cnitin041 <70575293+cnitin041@users.noreply.github.com>
2024-05-25 00:51:01 +0530

    baiary search

--- Binaryyy.cpp
+++ Binaryyy.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-//binary 
+//binary search
 int bs(int arr[],int n,int k){
     int start = 0;
     int end = n-1;
-    int mid=(start+end)/2;
+    int mid=start+(end+start)/2;//to avoid over flow
 
     while(start<=end){
         if(arr[mid]==k){
@@ -21,7 +21,7 @@ int bs(int arr[],int n,int k){
            
         }
 
-        mid=(start+end)/2;
+        int mid=start+(end+start)/2;
     }
 
     return -1;
