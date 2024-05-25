0262d68585b28c8a136cbcc6fc0e02e382074188
cnitin041 <70575293+cnitin041@users.noreply.github.com>
2024-05-26 00:58:52 +0530

    baiary search

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 
 
-
-int bs(int arr[],int n,int k){
-    int start=0;
-    int end=n-1;
-    int mid=start+(end-start)/2;
-    while(start<=end){
-        if(arr[mid]==k){
-            return mid;
-        }
-        else if(arr[mid]>k){
-            end=mid-1;
-        }
-        else{
-            start=mid+1;
-        }
-
-        mid=start+(end-start)/2;
-    }
-    return -1;
-}
-
 int main(){
-    int arr[5]={1,2,3,5,7};
-    int index=bs(arr,5,10);
-    cout<<index;
+    
+    int a,b;
+    cin>>a>>b;
+    int arr[a][b];
+
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+    for(int i=b-1;i>=0;i--){
+        for(int j=0;j<a;j++){
+            cout<<arr[i++][j++];
+        }
+        cout<<endl;
+    }
+    
+    
+    
 }
+
