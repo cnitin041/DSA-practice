e406e0244d156977b744bf3256cd6c3b1fe4fd2d
cnitin041 <70575293+cnitin041@users.noreply.github.com>
2024-05-26 00:28:27 +0530

    baiary search

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,35 +1,30 @@
 #include<iostream>
 using namespace std;
 
-//binary 
-int bs(int arr[],int n,int k){
-    int start = 0;
-    int end = n-1;
-    int mid=(start+end)/2;
 
+
+int bs(int arr[],int n,int k){
+    int start=0;
+    int end=n-1;
+    int mid=start+(end-start)/2;
     while(start<=end){
         if(arr[mid]==k){
             return mid;
-           
         }
-        if(k>arr[mid]){
-            start=mid+1;
-             
+        else if(arr[mid]>k){
+            end=mid-1;
         }
         else{
-            end=mid-1;
-           
+            start=mid+1;
         }
 
-        mid=(start+end)/2;
+        mid=start+(end-start)/2;
     }
-
     return -1;
-
 }
 
 int main(){
     int arr[5]={1,2,3,5,7};
-    int index=bs(arr,5,5);
+    int index=bs(arr,5,10);
     cout<<index;
 }
